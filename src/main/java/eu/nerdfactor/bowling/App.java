bf3a39c7807027db058f73c64d769132d4658a5f
Daniel Klug <mail@nerdfactor.eu>
2023-12-04 10:13:17 +0100

    basic spring boot dependencies.

---
+++ src/main/java/eu/nerdfactor/bowling/App.java
@@ -0,0 +1,13 @@
+package eu.nerdfactor.bowling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}

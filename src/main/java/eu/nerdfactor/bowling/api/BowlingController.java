26525d294a375d3fe50df653d6c30c49af29779d
Daniel Klug <mail@nerdfactor.eu>
2023-12-16 11:01:39 +0100

    refactor all actions to respond with the new DTO with HAL references.

--- src/main/java/eu/nerdfactor/bowling/api/BowlingController.java
+++ src/main/java/eu/nerdfactor/bowling/api/BowlingController.java
@@ -1,20 +1,25 @@
 package eu.nerdfactor.bowling.api;
 
+import eu.nerdfactor.bowling.dto.BowlingGameDto;
 import eu.nerdfactor.bowling.entity.BowlingGame;
 import eu.nerdfactor.bowling.exceptions.MaxAmountOfRollsExceededException;
 import eu.nerdfactor.bowling.exceptions.WrongAmountOfPinsException;
+import eu.nerdfactor.bowling.service.BowlingGameCrudService;
 import eu.nerdfactor.bowling.service.BowlingService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Slf4j
 @RestController
@@ -23,34 +28,50 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Bowling", description = "Bowling Game Api")
 public class BowlingController {
 
+	private final ModelMapper modelMapper;
 	private final BowlingService bowlingService;
+	private final BowlingGameCrudService bowlingGameCrudService;
 
-	@PostMapping("/{id}/roll/{pins}")
+	@PostMapping(value = "/start", produces = "application/hal+json")
+	@Operation(
+			summary = "Start a new Bowling Game.",
+			description = "Start a new Bowling Game that can be used to execute rolls and calculate scores."
+	)
+	@ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = BowlingGameDto.class), mediaType = "application/hal+json")})
+	public ResponseEntity<BowlingGameDto> startGame() {
+		BowlingGame game = new BowlingGame();
+		this.bowlingGameCrudService.createGame(game);
+		BowlingGameDto dto = this.modelMapper.map(game, BowlingGameDto.class);
+		dto.add(linkTo(methodOn(BowlingRestController.class).readGame(dto.getId())).withSelfRel());
+		return ResponseEntity.ok(dto);
+	}
+
+	@PostMapping(value = "/{id}/roll/{pins}", produces = "application/hal+json")
 	@Operation(
 			summary = "Execute the next roll of a Bowling Game.",
 			description = "The next roll of a Bowling Game can be executed by providing the Id of the specific game and the pins that should be next over by the roll."
 	)
-	@ApiResponses({
-			@ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = BowlingGame.class), mediaType = "application/json")}),
-			@ApiResponse(responseCode = "404", content = {@Content()})
-	})
-	public ResponseEntity<BowlingGame> nextRoll(@PathVariable int id, @PathVariable(name = "pins") int knockedOverPins)
+	@ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = BowlingGameDto.class), mediaType = "application/hal+json")})
+	@ApiResponse(responseCode = "404", content = {@Content()})
+	public ResponseEntity<BowlingGameDto> nextRoll(@PathVariable int id, @PathVariable(name = "pins") int knockedOverPins)
 			throws EntityNotFoundException, WrongAmountOfPinsException, MaxAmountOfRollsExceededException {
 		BowlingGame game = this.bowlingService.addNextRoll(id, knockedOverPins);
-		return ResponseEntity.ok(game);
+		BowlingGameDto dto = this.modelMapper.map(game, BowlingGameDto.class);
+		dto.add(linkTo(methodOn(BowlingRestController.class).readGame(dto.getId())).withSelfRel());
+		return ResponseEntity.ok(dto);
 	}
 
-	@GetMapping("/{id}/score")
+	@GetMapping(value = "/{id}/score", produces = "application/hal+json")
 	@Operation(
 			summary = "Calculate the score of a Bowling Game.",
 			description = "The score of a Bowling Game can be calculated by providing the Id of the specific game. The score will be calculated for the current state of the game. It does not have to be finished to be scored."
 	)
-	@ApiResponses({
-			@ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = BowlingGame.class), mediaType = "application/json")}),
-			@ApiResponse(responseCode = "404", content = {@Content()})
-	})
-	public ResponseEntity<BowlingGame> calculateScore(@PathVariable int id) throws EntityNotFoundException {
+	@ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = BowlingGameDto.class), mediaType = "application/hal+json")})
+	@ApiResponse(responseCode = "404", content = {@Content()})
+	public ResponseEntity<BowlingGameDto> calculateScore(@PathVariable int id) throws EntityNotFoundException {
 		BowlingGame game = this.bowlingService.calculateCurrentScore(id);
-		return ResponseEntity.ok(game);
+		BowlingGameDto dto = this.modelMapper.map(game, BowlingGameDto.class);
+		dto.add(linkTo(methodOn(BowlingRestController.class).readGame(dto.getId())).withSelfRel());
+		return ResponseEntity.ok(dto);
 	}
 }
--- src/main/java/eu/nerdfactor/bowling/api/BowlingRestController.java
+++ src/main/java/eu/nerdfactor/bowling/api/BowlingRestController.java
@@ -1,22 +1,25 @@
 package eu.nerdfactor.bowling.api;
 
+import eu.nerdfactor.bowling.dto.BowlingGameDto;
 import eu.nerdfactor.bowling.entity.BowlingGame;
 import eu.nerdfactor.bowling.service.BowlingGameCrudService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Slf4j
 @RestController
@@ -25,57 +28,62 @@ import java.util.Optional;
 @Tag(name = "Bowling", description = "Bowling Game Api")
 public class BowlingRestController {
 
+	private final ModelMapper modelMapper;
 	private final BowlingGameCrudService bowlingGameCrudService;
 
-	@GetMapping
+	@GetMapping(produces = "application/hal+json")
 	@Operation(
 			summary = "Get a list of Bowling Games.",
 			description = "Get a list of all Bowling Games."
 	)
-	@ApiResponses({
-			@ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = BowlingGame.class), mediaType = "application/json")}),
-	})
-	public ResponseEntity<List<BowlingGame>> listGames() {
-		return ResponseEntity.ok(this.bowlingGameCrudService.listGames());
+	@ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = BowlingGameDto.class), mediaType = "application/hal+json")})
+	public ResponseEntity<List<BowlingGameDto>> listGames() {
+		return ResponseEntity.ok(this.bowlingGameCrudService.listGames().stream()
+				.map(bowlingGame -> {
+					BowlingGameDto dto = this.modelMapper.map(bowlingGame, BowlingGameDto.class);
+					dto.add(linkTo(methodOn(BowlingRestController.class).readGame(dto.getId())).withSelfRel());
+					return dto;
+				}).toList());
 	}
 
-	@GetMapping("/{id}")
+	@GetMapping(value = "/{id}", produces = "application/hal+json")
 	@Operation(
 			summary = "Get a Bowling Game.",
 			description = "Get a Bowling Game specified by the provided id."
 	)
-	@ApiResponses({
-			@ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = BowlingGame.class), mediaType = "application/json")}),
-	})
-	public ResponseEntity<BowlingGame> readGame(@PathVariable int id) {
-		Optional<BowlingGame> read = this.bowlingGameCrudService.readGame(id);
-		return ResponseEntity.of(read);
+	@ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = BowlingGameDto.class), mediaType = "application/hal+json")})
+	public ResponseEntity<BowlingGameDto> readGame(@PathVariable int id) {
+		BowlingGameDto dto = this.bowlingGameCrudService.readGame(id)
+				.map(entity -> this.modelMapper.map(entity, BowlingGameDto.class))
+				.orElseThrow();
+		dto.add(linkTo(methodOn(BowlingRestController.class).readGame(dto.getId())).withSelfRel());
+		return ResponseEntity.ok(dto);
 	}
 
-	@PostMapping
+	@PostMapping(produces = "application/hal+json")
 	@Operation(
 			summary = "Create a Bowling Game.",
 			description = "Create a new Bowling Game with the provided data about a Bowling Game."
 	)
-	@ApiResponses({
-			@ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = BowlingGame.class), mediaType = "application/json")}),
-	})
-	public ResponseEntity<BowlingGame> createGame(@RequestBody @Valid BowlingGame game) {
+	@ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = BowlingGameDto.class), mediaType = "application/hal+json")})
+	public ResponseEntity<BowlingGameDto> createGame(@RequestBody @Valid BowlingGame game) {
 		BowlingGame created = this.bowlingGameCrudService.createGame(game);
-		return ResponseEntity.ok(created);
+		BowlingGameDto dto = this.modelMapper.map(created, BowlingGameDto.class);
+		dto.add(linkTo(methodOn(BowlingRestController.class).readGame(dto.getId())).withSelfRel());
+		return ResponseEntity.ok(dto);
 	}
 
-	@PutMapping("/{id}")
+	@PutMapping(value = "/{id}", produces = "application/hal+json")
 	@Operation(
 			summary = "Update a Bowling Game.",
 			description = "Update a Bowling game with the provided data and specified id."
 	)
-	@ApiResponses({
-			@ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = BowlingGame.class), mediaType = "application/json")}),
-	})
-	public ResponseEntity<BowlingGame> updateGame(@PathVariable int id, @RequestBody @Valid BowlingGame game) {
+	@ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = BowlingGameDto.class), mediaType = "application/hal+json")})
+	public ResponseEntity<BowlingGameDto> updateGame(@PathVariable int id, @RequestBody @Valid BowlingGame game) {
 		BowlingGame updated = this.bowlingGameCrudService.updateGame(game);
-		return ResponseEntity.ok(updated);
+		BowlingGameDto dto = this.modelMapper.map(updated, BowlingGameDto.class);
+		dto.add(linkTo(methodOn(BowlingRestController.class).readGame(dto.getId())).withSelfRel());
+		return ResponseEntity.ok(dto);
 	}
 
 	@DeleteMapping("/{id}")
@@ -83,9 +91,7 @@ public class BowlingRestController {
 			summary = "Delete a Bowling Game.",
 			description = "Delete a Bowling Game specified by the id."
 	)
-	@ApiResponses({
-			@ApiResponse(responseCode = "204", content = {@Content()})
-	})
+	@ApiResponse(responseCode = "204", content = {@Content()})
 	public ResponseEntity<Void> deleteGame(@PathVariable int id) {
 		this.bowlingGameCrudService.deleteGameById(id);
 		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();

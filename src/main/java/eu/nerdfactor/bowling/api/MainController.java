5d1c32df5fb6a6d3768d1fd06becbc0848062c90
Daniel Klug <mail@nerdfactor.eu>
2023-12-16 11:00:11 +0100

    add basic hateoas response to api entry point. reorganise the api endpoints to redirect to the new entry point. add basic test to check if the hateoas information is correctly send from the contoller.

--- src/main/java/eu/nerdfactor/bowling/api/MainController.java
+++ src/main/java/eu/nerdfactor/bowling/api/MainController.java
@@ -9,12 +9,18 @@ import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @Tag(name = "Bowling", description = "Bowling Game Api")
@@ -30,9 +36,9 @@ public class MainController {
 
 	/**
 	 * The main endpoint of the application should just show a life sign
-	 * so that consumers know that there is something behind the api.
+	 * so that consumers know that there is something behind the server.
 	 *
-	 * @return ResponseEntity with HTTP 200.
+	 * @return {@link ResponseEntity} with {@link HttpStatus} 200.
 	 */
 	@GetMapping(value = {"", "/"})
 	@Hidden
@@ -40,20 +46,46 @@ public class MainController {
 		return ResponseEntity.ok().build();
 	}
 
+
 	/**
-	 * A simple status endpoint to show more information about the api.
+	 * The main api entrypoint will return some status information about the api.
+	 * It also contains links for clients to follow and start using the application.
 	 *
-	 * @return ResponseEntity with HTTP 200 and basic status info.
+	 * @return {@link ApiStatus} with basic status information.
 	 */
 	@Operation(
-			summary = "Retrieve status information about the Api",
-			description = "The endpoint returns status information about theApi. This should help to find out if the Api is running and functioning. In addition it will contain more information about the Api (version, spec etc.)."
+			summary = "Retrieve basic information about the Api",
+			description = "The endpoint returns basic information about the Api. This should help to find out if the Api is running and functioning. In addition it will contain more information about the Api (version, spec etc.)."
 	)
-	@ApiResponses({
-			@ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = ApiStatus.class), mediaType = "application/json")})
-	})
-	@GetMapping(value = "/api/v1/status", produces = "application/json")
-	public ResponseEntity<ApiStatus> status() {
-		return ResponseEntity.ok(new ApiStatus());
+	@ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = ApiStatus.class), mediaType = "application/hal+json")})
+	@GetMapping(value = "/api/v1", produces = "application/hal+json")
+	public ResponseEntity<ApiStatus> api() {
+		ApiStatus model = new ApiStatus();
+
+		model.add(linkTo(methodOn(MainController.class).api()).withSelfRel());
+		model.add(linkTo(methodOn(MainController.class).api()).slash("docs").withRel("docs"));
+		model.add(linkTo(methodOn(MainController.class).api()).slash("swagger").withRel("swagger"));
+		model.add(linkTo(methodOn(BowlingController.class).startGame()).withRel("startBowlingGame"));
+		model.add(linkTo(methodOn(BowlingRestController.class).listGames()).withRel("listBowlingGames"));
+
+		return ResponseEntity.ok(model);
+	}
+
+	/**
+	 * The old status endpoint now redirects to the Api entry endpoint.
+	 */
+	@GetMapping(value = "/api/v1/status")
+	@Hidden
+	public void status(HttpServletResponse response) throws IOException {
+		response.sendRedirect("/api/v1");
+	}
+
+	/**
+	 * The Api endpoint without version redirects to the current versioned entry endpoint.
+	 */
+	@GetMapping(value = "/api")
+	@Hidden
+	public void apiWithoutVersion(HttpServletResponse response) throws IOException {
+		response.sendRedirect("/api/v1");
 	}
 }
--- src/test/java/eu/nerdfactor/bowling/api/MainControllerTest.java
+++ src/test/java/eu/nerdfactor/bowling/api/MainControllerTest.java
@@ -2,6 +2,7 @@ package eu.nerdfactor.bowling.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.nerdfactor.bowling.dto.ApiStatus;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,7 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -27,8 +27,14 @@ class MainControllerTest {
 	 */
 	@Test
 	void applicationStatusCanBeRequested() throws Exception {
-		mockMvc.perform(get("/api/v1/status"))
+		String response = mockMvc.perform(get("/api/v1"))
 				.andExpect(status().isOk())
-				.andExpect(content().json(this.jsonMapper.writeValueAsString(new ApiStatus())));
+				.andReturn()
+				.getResponse()
+				.getContentAsString();
+		ApiStatus status = this.jsonMapper.readValue(response, ApiStatus.class);
+		Assertions.assertTrue(status.isStatus());
+		Assertions.assertTrue(status.hasLinks());
+		Assertions.assertTrue(status.hasLink("swagger"));
 	}
 }
